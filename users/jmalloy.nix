80aa37e4b851981dfc92a4de4e9bffa88db9ddc1
Jacob Malloy <jmalloy1@vols.utk.edu>
2024-02-21 22:25:26 -0500

    add hashed password to config

--- users/jmalloy.nix
+++ users/jmalloy.nix
@@ -6,6 +6,7 @@
     isNormalUser = true;
     description = "Jacob Malloy";
     shell = pkgs.zsh;
+    hashedPassword="$6$Mt1..jrvHr8Zc0zV$UPOUV3Ui4FBtoQcN7SsqybBvDwzSasbKnHlOfNtLgQ1WCUK2F7RU3tGUfHKiBBFZ7BZGlRrjqzW2io0gxlGpk/";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
     ];
