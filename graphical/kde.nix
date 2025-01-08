dbaeca3b1f2bdc6d14272d221f5d2a5a13b3899e
Jacob Malloy <jmalloy1@vols.utk.edu>
2025-01-08 18:45:45 -0500

    add kde

---
+++ graphical/kde.nix
@@ -0,0 +1,24 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  
+
+
+
+
+
+
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
